8d3e9e59961f9c34761027347ee7b8427d4fcc2e
sydneykwok <sydneykwok@gmail.com>
2023-02-01 22:29:00 -0700

    set up basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import PersonalBoardPage from "./pages/PersonalBoardPage";
+import ProfilePage from "./pages/ProfilePage";
+import WishingCenterPage from "./pages/WishingCenterPage";
+
 function App() {
   return (
     <div className="app">
-      Hello World
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<LoginPage/>}/>
+                <Route path={"/home"} element={<HomePage/>}/>
+                <Route path={"/personal-board"} element={<PersonalBoardPage/>}/>
+                <Route path={"/profile"} element={<ProfilePage/>}/>
+                <Route path={"/wishing-center"} element={<WishingCenterPage/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
