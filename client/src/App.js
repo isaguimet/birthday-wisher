aa794a712b2eb7935656d9bbf593eebb9a8cfacc
sydneykwok <sydneykwok@gmail.com>
2023-02-02 22:08:50 -0700

    run code formatter on all js files

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,20 @@ import WishingCenterPage from "./pages/WishingCenterPage";
 import NavBar from "./components/NavBar";
 
 function App() {
-  return (
-    <div className="app">
-        <BrowserRouter>
-            <NavBar/>
-            <Routes>
-                <Route path={"/"} element={<LoginPage/>}/>
-                <Route path={"/home"} element={<HomePage/>}/>
-                <Route path={"/personal-board"} element={<PersonalBoardPage/>}/>
-                <Route path={"/profile"} element={<ProfilePage/>}/>
-                <Route path={"/wishing-center"} element={<WishingCenterPage/>}/>
-            </Routes>
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <NavBar/>
+                <Routes>
+                    <Route path={"/"} element={<LoginPage/>}/>
+                    <Route path={"/home"} element={<HomePage/>}/>
+                    <Route path={"/personal-board"} element={<PersonalBoardPage/>}/>
+                    <Route path={"/profile"} element={<ProfilePage/>}/>
+                    <Route path={"/wishing-center"} element={<WishingCenterPage/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
