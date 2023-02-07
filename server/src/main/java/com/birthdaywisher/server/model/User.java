6b1ac86958512de87aae478d81c57cb3351fabb5
isaguimet <isabellaguimet@hotmail.com>
2023-02-06 22:51:51 -0700

    Create User model

---
+++ server/src/main/java/com/birthdaywisher/server/model/User.java
@@ -0,0 +1,24 @@
+package com.birthdaywisher.server.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private LocalDate birthdate;
+
+    // TODO: Add rest of fields
+}
