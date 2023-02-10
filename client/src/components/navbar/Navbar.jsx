770e821c3ea7ef31689611d8c2134d0fbf3ff32b
JessieG123 <jessieguo863@gmail.com>
2023-02-09 23:53:03 -0700

    global styles kinda figured out

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import Theme from "../../theme/Theme";
+import { Container } from "./NavBar.style";
+
+const NavBar = () => {
+    return (
+        <Theme>
+        <Container>
+
+        hello
+
+        </Container>
+        </Theme>
+
+
+    );
+};
+
+export default NavBar;
