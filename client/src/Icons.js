348b55c4aea5db828c2ea44e0d8722215f36fa71
JessieG123 <jessieguo863@gmail.com>
2023-02-10 23:35:25 -0700

    added icons to nav bar

---
+++ client/src/Icons.js
@@ -0,0 +1,11 @@
+import homeIcon from './assets/home-icon.png';
+import friendsIcon from './assets/friends-icon.png';
+import notificationIcon from './assets/notification-icon.png';
+import profileIcon from './assets/profile-icon.png';
+
+export default [
+    homeIcon,
+    friendsIcon,
+    notificationIcon,
+    profileIcon
+];
--- client/src/components/navbar/Navbar.style.jsx
+++ client/src/components/navbar/Navbar.style.jsx
@@ -1,7 +1,21 @@
 import styled from 'styled-components';
-import Navbar from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-export const Container = styled(Navbar)`
-    background-color: ${props => props.theme.color.darkBlue};
-
+export const StyledNavbar = styled(Navbar)`
+    background-color: rgba(48, 49, 121, 0.92);
 `;
+
+export const StyledLogo = styled(Navbar.Brand)`
+    color: ${props => props.theme.color.ivory};
+`;
+
+// export const NavItems = styled(Nav)`
+//     justify-content: end;
+// `;
+
+export const Navlink = styled(Nav.Link)`
+    color: ${props => props.theme.color.ivory};
+`;
+
+// https://dev.to/aromanarguello/how-to-use-themes-in-styled-components-49h
