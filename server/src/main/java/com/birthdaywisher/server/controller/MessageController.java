d759ca6df35e4d7fd6aee2bc96d199ca59a5a557
sydneykwok <sydneykwok@gmail.com>
2023-02-10 23:17:11 -0700

    create message endpoints for crud operations

--- server/src/main/java/com/birthdaywisher/server/controller/MessageController.java
+++ server/src/main/java/com/birthdaywisher/server/controller/MessageController.java
@@ -2,22 +2,56 @@ package com.birthdaywisher.server.controller;
 
 import com.birthdaywisher.server.model.Message;
 import com.birthdaywisher.server.service.MessageService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
     @Autowired
     private MessageService messageService;
-    @GetMapping
-    public ResponseEntity<List<Message>> getMessages() {
-        return new ResponseEntity<List<Message>>(messageService.allMessages(), HttpStatus.OK);
+
+    @PostMapping
+    public ResponseEntity<Message> createMessage(@RequestBody Message msg) {
+        try {
+            return new ResponseEntity<Message>(messageService.createMessage(msg), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Message>> readMessage(@PathVariable ObjectId id) {
+        try {
+            return new ResponseEntity<Optional<Message>>(messageService.getMsgById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Message> updateMessage(@PathVariable ObjectId id, @RequestBody Map<String, String> payload) {
+        try {
+            return new ResponseEntity<Message>(messageService.updateMessage(id, payload), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteMessage(@PathVariable ObjectId id) {
+        try {
+            messageService.deleteMessage(id);
+            return new ResponseEntity<String>(
+                    "Message with id " + id + " has been successfully deleted.", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- server/src/main/java/com/birthdaywisher/server/service/MessageService.java
+++ server/src/main/java/com/birthdaywisher/server/service/MessageService.java
@@ -2,17 +2,33 @@ package com.birthdaywisher.server.service;
 
 import com.birthdaywisher.server.model.Message;
 import com.birthdaywisher.server.repository.MessageRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class MessageService {
-
     @Autowired
-    private MessageRepository messageRepository;
-    public List<Message> allMessages() {
-        return messageRepository.findAll();
+    private MessageRepository msgRepository;
+
+    public Message createMessage(Message msg) {
+        return msgRepository.save(msg);
+    }
+
+    public Optional<Message> getMsgById(ObjectId id) {
+        return msgRepository.findById(id);
+    }
+
+    public Message updateMessage(ObjectId id, Map<String, String> payload) {
+        Message msg = msgRepository.findById(id).get();
+        msg.setMsgText(payload.get("msgText"));
+        return msgRepository.save(msg);
+    }
+
+    public void deleteMessage(ObjectId id) {
+        msgRepository.deleteById(id);
     }
 }
