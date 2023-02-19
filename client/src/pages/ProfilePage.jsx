23744ea9693e2f4007c739ba18e931acbb1f428c
Jane <jshen019@gmail.com>
2023-02-18 19:49:38 -0700

    added button component and fixed typo in nav bar

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,18 +1,19 @@
 import avatar from "../assets/avatar.png";
 import "./ProfilePage.css";
+import PrimaryButton from "../components/button/PrimaryButton";
 
 const ProfilePage = () => {
 
     return (
         <div className="container">
-            <img src={avatar} alt="Avatar"></img>
+            <img className="profile-picture" src={avatar} alt="Avatar"></img>
             <div className="right-container">
                 <h1>Bobby Bob</h1>
                 <p className="title">Email</p>
                 <p>Bobby@gmail.com</p>
                 <p className="title">Birthday</p>
                 <p>January 1, 2000</p>
-                <button className="button">Edit</button>
+                <PrimaryButton buttonText="Edit"></PrimaryButton>
             </div>
         </div>
     );
