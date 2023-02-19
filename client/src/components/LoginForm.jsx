a4cb37594551b5ffb024ff9debcdcbbd15546e2e
sydneykwok <sydneykwok@gmail.com>
2023-02-19 12:54:48 -0700

    re-organize redux store

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -4,7 +4,7 @@ import {Formik} from "formik";
 import * as yup from "yup";
 import {useNavigate} from "react-router-dom";
 import {useDispatch} from "react-redux";
-import {setLogin} from "../state";
+import {setLogin} from "../store/user";
 
 // yup validation schemas
 const registerSchema = yup.object().shape({
--- client/src/index.js
+++ client/src/index.js
@@ -3,24 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import authReducer from "./state";
-import {configureStore} from "@reduxjs/toolkit";
+import store from "./store";
 import {Provider} from "react-redux";
-import {FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE} from "redux-persist";
-import storage from "redux-persist/lib/storage"
+import {persistStore} from "redux-persist";
 import {PersistGate} from "redux-persist/integration/react";
-
-const persistConfig = {key: "root", storage, version: 1};
-const persistedReducer = persistReducer(persistConfig, authReducer);
-const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
-});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
