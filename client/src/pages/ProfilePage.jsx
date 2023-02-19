4660ac8c947868ac2571ddfcfbe2ebccdef01718
sydneykwok <sydneykwok@gmail.com>
2023-02-19 12:06:19 -0700

    run code formatter on ProfilePage.jsx

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,12 +2,12 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Theme from "../theme/Theme";
-import { StyledDiv, Profile, Board, BoardContainer} from "./ProfilePage.style"
+import {StyledDiv, Profile, Board, BoardContainer} from "./ProfilePage.style"
 import BirthdayCard from '../components/birthdayCard/BirthdayCard';
 import ProfilePic from '../components/profilePic/ProfilePic';
 import Icons from '../Icons';
 import ListWishes from '../components/listWishes/ListWishes';
-import { useState } from 'react';
+import {useState} from 'react';
 
 
 const ProfilePage = (props) => {
@@ -19,69 +19,69 @@ const ProfilePage = (props) => {
 
     return (
         <Theme>
-        <StyledDiv>
-            <Profile>
-            
-            <Container>
-                <Row>
-                    <Col><ProfilePic src={Icons[4]}/></Col>
-                    <Col>
-                        <Container>
-                        <Row>
-                            <h1>John Doe 23</h1>
-                        </Row>
-                        <Row>
-                            <h4>January 1, 1999</h4>
-                        </Row>
-                        </Container>
-                    </Col>
-                    <Col>
-                        <h5>Settings</h5>
-                    </Col>
-                </Row>
-            </Container>
-            </Profile>
-            <BoardContainer>
-                2022
-                <button onClick={addWish}>Add Wish</button>
+            <StyledDiv>
+                <Profile>
 
-                       
-                {/* if no item it doesn't render anything, if no item it should just render blank */}
-            <Board>
-            {wishes.length ? wishes.map((item, i) => (<ListWishes wish={item} />)) : "no wishes"}
-                <BirthdayCard>
-                    Happy brithday!
-                </BirthdayCard>
-                <BirthdayCard>
-                    Happy brithday Joe!
-                </BirthdayCard>
-                <BirthdayCard>
-                    Happy brithday!
-                </BirthdayCard>
-                <BirthdayCard>
-                    Happy brithday Joe!
-                </BirthdayCard>
-            </Board>
-            </BoardContainer>
-            
-            <BoardContainer>
-                2021
-                <Board>
-                <BirthdayCard>
-                    Happy brithday!
-                </BirthdayCard>
-                <BirthdayCard>
-                    Happy brithday Joe!
-                </BirthdayCard>
-                <BirthdayCard>
-                    Happy brithday!
-                </BirthdayCard>
-                <BirthdayCard>
-                    Happy brithday Joe!
-                </BirthdayCard>
-                </Board>
-            </BoardContainer>
-        </StyledDiv>
+                    <Container>
+                        <Row>
+                            <Col><ProfilePic src={Icons[4]}/></Col>
+                            <Col>
+                                <Container>
+                                    <Row>
+                                        <h1>John Doe 23</h1>
+                                    </Row>
+                                    <Row>
+                                        <h4>January 1, 1999</h4>
+                                    </Row>
+                                </Container>
+                            </Col>
+                            <Col>
+                                <h5>Settings</h5>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Profile>
+                <BoardContainer>
+                    2022
+                    <button onClick={addWish}>Add Wish</button>
+
+
+                    {/* if no item it doesn't render anything, if no item it should just render blank */}
+                    <Board>
+                        {wishes.length ? wishes.map((item, i) => (<ListWishes wish={item}/>)) : "no wishes"}
+                        <BirthdayCard>
+                            Happy brithday!
+                        </BirthdayCard>
+                        <BirthdayCard>
+                            Happy brithday Joe!
+                        </BirthdayCard>
+                        <BirthdayCard>
+                            Happy brithday!
+                        </BirthdayCard>
+                        <BirthdayCard>
+                            Happy brithday Joe!
+                        </BirthdayCard>
+                    </Board>
+                </BoardContainer>
+
+                <BoardContainer>
+                    2021
+                    <Board>
+                        <BirthdayCard>
+                            Happy brithday!
+                        </BirthdayCard>
+                        <BirthdayCard>
+                            Happy brithday Joe!
+                        </BirthdayCard>
+                        <BirthdayCard>
+                            Happy brithday!
+                        </BirthdayCard>
+                        <BirthdayCard>
+                            Happy brithday Joe!
+                        </BirthdayCard>
+                    </Board>
+                </BoardContainer>
+            </StyledDiv>
         </Theme>
     );
 };
