aad269a6dbf6daf0fffe8d2607c25fb7f2781aa1
sydneykwok <sydneykwok@gmail.com>
2023-02-20 20:25:38 -0700

    remove unnecessary import

--- server/src/main/java/com/birthdaywisher/server/repository/BoardRepository.java
+++ server/src/main/java/com/birthdaywisher/server/repository/BoardRepository.java
@@ -1,7 +1,6 @@
 package com.birthdaywisher.server.repository;
 
 import com.birthdaywisher.server.model.Board;
-import com.birthdaywisher.server.model.Message;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
