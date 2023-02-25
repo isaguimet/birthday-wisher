3c923d9ba6618722631f6025cc218f7e174fe503
ViTsang <47119514+ViTsang@users.noreply.github.com>
2023-02-25 12:51:02 -0700

    changed login form

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,162 +1,98 @@
-import {useState} from "react";
-import {Button, TextField, Typography} from "@mui/material";
-import {Formik} from "formik";
-import * as yup from "yup";
-import {useNavigate} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {setLogin} from "../state";
+import React, { useState } from "react"
+import DatePicker from "./DatePicker";
+import "./LoginStyle.css";
 
-// yup validation schemas
-const registerSchema = yup.object().shape({
-    firstName: yup.string().required("required"),
-    lastName: yup.string().required("required"),
-    email: yup.string().email("invalid email").required("required"),
-    password: yup.string().required("required"),
-    // TODO: add birthday
-});
+export default function (props) {
+  let [authMode, setAuthMode] = useState("signin")
 
-const loginSchema = yup.object().shape({
-    email: yup.string().email("invalid email").required("required"),
-    password: yup.string().required("required"),
-});
+  const changeAuthMode = () => {
+    setAuthMode(authMode === "signin" ? "signup" : "signin")
+  }
 
-// initial values
-const initialValuesRegister = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    // TODO: add birthday
-};
-
-const initialValuesLogin = {
-    email: "",
-    password: "",
-};
-
-const LoginForm = () => {
-
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    const [formType, setFormType] = useState("login");
-    const isLogin = formType === "login";
-    const isRegister = formType === "register";
-
-    const register = async (values, onSubmitProps) => {
-        // TODO: make API call to create new user in db
-        onSubmitProps.resetForm();
-        setFormType("login");
-    };
-
-    const login = async (values, onSubmitProps) => {
-        // TODO: make API call to authenticate user
-        onSubmitProps.resetForm();
-        dispatch(
-            setLogin({
-                user: values.email
-            })
-        );
-        navigate("/home");
-    };
-
-    const handleFormSubmit = async (values, onSubmitProps) => {
-        if (isLogin) await login(values, onSubmitProps);
-        if (isRegister) await register(values, onSubmitProps);
-    };
-
+  if (authMode === "signin") {
     return (
-        <Formik
-            onSubmit={handleFormSubmit}
-            initialValues={isLogin ? initialValuesLogin : initialValuesRegister}
-            validationSchema={isLogin ? loginSchema : registerSchema}
-        >
-            {({
-                  values,
-                  errors,
-                  touched,
-                  handleBlur,
-                  handleChange,
-                  handleSubmit,
-                  setFieldValue,
-                  resetForm
-              }) => (
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        {isRegister && (
-                            <>
-                                <TextField
-                                    label={"First Name"}
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    value={values.firstName}
-                                    name={"firstName"}
-                                    error={Boolean(touched.firstName) && Boolean(errors.firstName)}
-                                    helperText={touched.firstName && errors.firstName}
-                                />
-                                <TextField
-                                    label={"Last Name"}
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    value={values.lastName}
-                                    name={"lastName"}
-                                    error={Boolean(touched.lastName) && Boolean(errors.lastName)}
-                                    helperText={touched.lastName && errors.lastName}
-                                />
-                            </>
-                        )}
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Sign In</h3>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit">
+                Submit
+              </button>
+              <div className="text-center">
+              <span className="link-primary" onClick={changeAuthMode}>
+                Create an Account
+              </span>
+            </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
 
-                        {/* Section common to both login & register */}
-                        <TextField
-                            label={"Email"}
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.email}
-                            name={"email"}
-                            error={Boolean(touched.email) && Boolean(errors.email)}
-                            helperText={touched.email && errors.email}
-                        />
-                        <TextField
-                            label={"Password"}
-                            type={"password"}
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                            value={values.password}
-                            name={"password"}
-                            error={Boolean(touched.password) && Boolean(errors.password)}
-                            helperText={touched.password && errors.password}
-                        />
-                    </div>
-
-                    {/* BUTTONS */}
-                    <div>
-                        <Button
-                            fullWidth
-                            type={"submit"}
-                        >
-                            {isLogin ? "LOGIN" : "REGISTER"}
-                        </Button>
-                        <Typography
-                            onClick={() => {
-                                setFormType(isLogin ? "register" : "login");
-                                resetForm();
-                            }}
-                            sx={{
-                                textDecoration: "underline",
-                                "&:hover": {
-                                    cursor: "pointer",
-                                },
-                            }}
-                        >
-                            {isLogin
-                                ? "Don't have an account? Sign up here."
-                                : "Already have an account? Login here."}
-                        </Typography>
-                    </div>
-                </form>
-            )}
-        </Formik>
-    );
-};
-
-export default LoginForm;
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign Up</h3>
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
+              type="name"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Email Address"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Birthday</label>
+            <DatePicker></DatePicker>
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit">
+              Submit
+            </button>
+          </div>
+          <div className="text-center">
+            Have an account?{" "}
+            <span className="link-primary" onClick={changeAuthMode}>
+              Sign In
+            </span>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
