c006e0eb28e881598555e9af21536b4141521692
Jane <jshen019@gmail.com>
2023-02-28 16:15:36 -0700

    Linked backend to frontend for wishing center page

--- client/src/pages/WishingCenterPage.jsx
+++ client/src/pages/WishingCenterPage.jsx
@@ -1,35 +1,112 @@
-import "./WishingCenterPage.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Alert } from "reactstrap";
 import Table from 'react-bootstrap/Table';
+import { StyledDiv } from "./WishingCenterPage.style";
 
-const WishingCenterPage = () => {
+const Row = (props) => {
+    const { firstName, lastName, birthdate } = props
+    var months = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    const month = months[parseInt(birthdate.slice(5, 7)) - 1];
+    const day = birthdate.slice(8);
 
     return (
-        <div className="wishing-center-container">
-            <h1>Upcoming Birthdays</h1>
+        <tr>
+            <td>{month} {day}</td>
+            <td>{firstName} {lastName}'s Birthday</td>
+        </tr>
+    )
+}
+
+const BirthdayTable = (props) => {
+    const { data } = props
+    return (
+        <div>
             <Table striped bordered hover>
                 <tbody>
-                    <tr>
-                        <td>February 12</td>
-                        <td>John's Birthday</td>
-                    </tr>
-                    <tr>
-                        <td>February 23</td>
-                        <td>Tom's Birthday</td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>Lisa's Birthday</td>
-                    </tr>
-                    <tr>
-                        <td>March 14</td>
-                        <td>Bobby's Birthday</td>
-                    </tr>
-                    <tr>
-                        <td>March 26</td>
-                        <td>Alice's Birthday</td>
-                    </tr>
+                    {data.map(row =>
+                        <Row firstName={row.firstName}
+                            lastName={row.lastName}
+                            birthdate={row.birthdate} />
+                    )}
                 </tbody>
             </Table>
+        </div>
+    )
+}
+
+function filterUpcomingBirthdays(data) {
+    var today = new Date();
+    var day = today.getDate();
+    var month = today.getMonth() + 1;
+    data.map(friend => console.log(" " + friend.birthdate.slice(5, 7) + " " + friend.birthdate.slice(8)));
+    return data.filter(friend => parseInt(friend.birthdate.slice(5, 7)) > month || (parseInt(friend.birthdate.slice(5, 7)) === month && parseInt(friend.birthdate.slice(8))) >= day);
+}
+
+// comparator to sort birthdays in ascending order by day and month
+function compare(a, b) {
+    if (a.birthdate.slice(5, 7) < b.birthdate.slice(5, 7)) {
+        return -1;
+    } if (a.birthdate.slice(5, 7) > b.birthdate.slice(5, 7)) {
+        return 1;
+    } if (a.birthdate.slice(5, 7) === b.birthdate.slice(5, 7)) {
+        if (a.birthdate.slice(8) < b.birthdate.slice(8)) {
+            return -1;
+        } if (a.birthdate.slice(8) > b.birthdate.slice(8)) {
+            return 1;
+        }
+        return 0;
+    }
+}
+
+const WishingCenterPage = () => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState(null);
+    // const [nextyearBirthdays, setNextYearBirthdays] = useState(null);
+    const [error, setError] = useState(null);
+
+    const userId = "63f90f25dca51a6d00d65007";
+
+    useEffect(() => {
+        setLoading(true);
+        axios.get(`http://localhost:8080/users/friendList/${userId}`).then((response) => {
+            setLoading(false);
+            setData(filterUpcomingBirthdays(response.data.sort(compare)));
+            // setNextYearBirthdays(response.data.sort(compare));
+            console.log("response data: " + JSON.stringify(response.data))
+            setError(null);
+        }).catch((err) => {
+            setLoading(false);
+            if (err.response) {
+                setError(err.response.data);
+            } else {
+                setError(err.message);
+            }
+            console.log(error)
+        });
+    }, []);
+
+    const handleAlertToggle = () => {
+        setError(null);
+    }
+
+    return (
+        <div>
+            {!!error && (
+                <Alert color={"danger"} toggle={handleAlertToggle}>
+                    Error: {error}
+                </Alert>
+            )}
+            {loading && <div>Loading Wishing Center...</div>}
+            {!loading && data ? (
+                <StyledDiv>
+                    <h1>Upcoming Birthdays</h1>
+                    <BirthdayTable data={data} />
+                    {/* <h1> Next Year Birthdays</h1>
+                    <BirthdayTable data={nextyearBirthdays} /> */}
+                </StyledDiv>
+            ) : null}
         </div>
     );
 };
