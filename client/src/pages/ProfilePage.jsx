34b7a717e3e97f131b372f9f3c9ad2432a657cd0
Jane <jshen019@gmail.com>
2023-03-02 23:42:07 -0700

    removed old code on ProfilePage

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,19 +1,7 @@
-import avatar from "../assets/avatar.png";
-import "./ProfilePage.css";
-
 const ProfilePage = () => {
 
     return (
-        <div className="container">
-            <img className="profile-picture" src={avatar} alt="Avatar"></img>
-            <div className="right-container">
-                <h1>Bobby Bob</h1>
-                <p className="title">Email</p>
-                <p>Bobby@gmail.com</p>
-                <p className="title">Birthday</p>
-                <p>January 1, 2000</p>
-            </div>
-        </div>
+        <h1>Profile Page</h1>
     );
 };
 
