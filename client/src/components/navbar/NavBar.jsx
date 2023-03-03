2c0150c5ab0e855e5ef0545967e45dbe0dc8bd13
JessieG123 <jessieguo863@gmail.com>
2023-03-03 11:39:46 -0700

    changed home to wishing center

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -3,15 +3,17 @@ import Theme from "../../theme/Theme";
 import {Navlink, StyledImg, StyledLogo, StyledNavbar} from "./NavBar.style";
 import Nav from 'react-bootstrap/Nav'
 import Icons from '../../Icons';
+import {useSelector} from "react-redux";
 
 const NavBar = () => {
+    const user = useSelector((state) => state.user);
     return (
         <Theme>
             <StyledNavbar>
                 <Container>
                     <StyledLogo href="/">Birthday Wisher</StyledLogo>
                     <Nav>
-                        <Navlink href="/home"><StyledImg src={Icons[0]}/></Navlink>
+                        <Navlink href="/wishing-center"><StyledImg src={Icons[0]}/></Navlink>
                         <Navlink href="/friends"><StyledImg src={Icons[1]}/></Navlink>
                         <Navlink href="/notification"><StyledImg src={Icons[2]}/></Navlink>
                         <Navlink href={`/profile/${user.id}`}><StyledImg src={Icons[3]}/></Navlink>
