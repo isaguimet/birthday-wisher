5c221733f66fa51e34ed6886acd5e942b741fe57
JessieG123 <jessieguo863@gmail.com>
2023-03-09 22:35:25 -0700

    socket and leader election

--- server/src/main/java/com/birthdaywisher/server/Application.java
+++ server/src/main/java/com/birthdaywisher/server/Application.java
@@ -1,5 +1,6 @@
 package com.birthdaywisher.server;
 
+import com.birthdaywisher.server.election.Server;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.bson.types.ObjectId;
 import org.springframework.boot.SpringApplication;
@@ -7,10 +8,97 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        try {
+            //--------------------------------- Common variabels ------------------------------------
+            InetAddress address = InetAddress.getByName("127.0.0.1");
+            
+            //initialize server information and setup successor and predecessor
+            // -------------------------------- BEGIN SERVER 1 SETUP --------------------------------
+            //server1 port: 5000
+            Server server = new Server(1, 3);
+            server.setSuccPort(5000);
+
+            Socket s = new Socket(address, server.getSuccPort());
+            server.setSuccSocket(s);
+            
+            // -------------------------------- END SERVER 1 SETUP --------------------------------
+            
+
+            // -------------------------------- BEGIN SERVER 2 SETUP --------------------------------
+            //server2 port: 6000
+            Server server = new Server(2, 3);
+            server.setSuccPort(6000);
+
+            Socket s = new Socket(address, server.getSuccPort());
+            server.setSuccSocket(s);
+            // -------------------------------- END SERVER 2 SETUP --------------------------------
+            
+
+            // -------------------------------- BEGIN SERVER 3 SETUP --------------------------------
+            //server3 port: 7000
+            Server server = new Server(3, 3);
+            server.setSuccPort(7000);
+
+            Socket s = new Socket(address, server.getSuccPort());
+            server.setSuccSocket(s);
+            
+            // -------------------------------- END SERVER 3 SETUP --------------------------------
+            //Leader functions
+            if (server.getServerId()== server.getLeaderId()) {
+                
+                //Send heart beat message to successor (server 3 to 1)
+                //open sockets 
+            } else {
+
+                //if no heart beat recieved (by 3) and timeout, server 1 start election
+
+            }
+
+            //sendMessageToSuccessor(Server server, String msg);
+
+
+            // InetAddress address = InetAddress.getByName("127.0.0.1");
+            // Socket s = new Socket(address, server1.getSuccPort());
+            // server1.setSuccSocket(s);
+            // DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            // dout.writeUTF("Hello Server 1");
+            // dout.flush();
+            // dout.close();
+            //whatever code here
+
+            s.close();
+
+            // all the codes
+
+
+
+
+            //listening to messages from server 1
+            //this is to create the server socket
+            ServerSocket serverSocket = new ServerSocket(5000);
+            Socket ss = serverSocket.accept();
+            System.out.println("SERVER 3");
+            DataInputStream dis = new DataInputStream(ss.getInputStream());
+            String  str=(String)dis.readUTF();
+            System.out.println("message= "+str);
+            ss.close();
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
         SpringApplication.run(Application.class, args);
     }
 
--- server/src/main/java/com/birthdaywisher/server/election/Server.java
+++ server/src/main/java/com/birthdaywisher/server/election/Server.java
@@ -1,67 +1,135 @@
+package com.birthdaywisher.server.election;
+
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class Server {
+    
     private int serverId;
-    public int serverIndex;
-    private int[] serverId;
-    private boolean active;
     private int predId;
-    private int succId;
-    private int predIndex;
-    private int succIndex;
-    private boolean running;
+    private Socket succSocket;
+    private int succPort;
+    private int leaderId;
+    private boolean isRunning;
 
-    Server(int index) {
-        this.index = index;
-        this.running = false;
-        setSuccessor(this.index);
-        setPedecessor(this.index);
+    public Server(int id) {
+        this.serverId = id;
+        this.isRunning = false;
     }
 
-    private void setSuccessor() {
-
-        int succ = this.index + 1;
-        if(succ < serverId.length) {
-            this.succId = serverId[succ];
-        } else {
-            this.succId = 0; //to back to begining
-        }
-
+    public Server(int id, int leaderId) {
+        this.serverId = id;
+        this.leaderId = leaderId;
+        this.isRunning = false;
     }
 
-    private void setPedecessor() {
-        int pred = this.index - 1;
-        if (pred > 0) {
-            this.predId = pred;
-        } else {
-            this.predId = serverId[serverId.length - 1];
-        }
-
-    }
-
-    //check if it's server exists
+     //Getters
     public int getServerId() {
         return this.serverId;
     }
-    public int getSuccId() {
-        return this.succId;
+
+    public Socket getSuccSocket() {
+        return this.succSocket;
     }
+
+    public int getSuccPort() {
+        return this.succPort;
+    }
+
     public int getPredId() {
         return this.predId;
     }
-    public int getServerIndex() {
-        return this.serverIndex;
-    }
-    public int getSuccIndex() {
-        return this.succIndex;
-    }
-    public int getPredIndex() {
-        return this.predIndex;
+
+    public boolean getIsRunning() {
+        return this.isRunning;
     }
 
-    public boolean getRunning() {
-        return this.running;
+    public int getLeaderId() {
+        return this.leaderId;
     }
 
-    public void setRunning(boolean status) {
-        this.running = status;
+    //Setters
+    public void setSuccSocket(Socket socket) {
+        this.succSocket = socket;
     }
+
+    public void setSuccPort(int port) {
+        this.succPort = port;
+    }
+
+    public void setPedecessor(int id) {
+        this.predId = id;
+    }
+
+    public void setLeaderId(int id) {
+        this.leaderId = id;
+    }
+
+    public void setIsRunning(boolean status) {
+        this.isRunning = status;
+    }
+
+    public void setLeader(int id) {
+        this.leaderId = id;
+    }
+
+    public void initiateElection() {
+        setIsRunning(true);
+        
+        System.out.println("Election initiated by server: " + getServerId());
+
+        election("0" + getServerId());
+    }
+ 
+    //call an election and select a new leader
+    // "03" election message and 3 is the server id running in the election
+    // 0 is election message
+    // 1 is leader message
+    public void election (String msg) {
+        char msgType = msg.charAt(0);
+        char msgId = msg.charAt(1);
+
+        if ((int) msgType == 0) {
+            //If message id is greater than server id, pass message onto successor
+            if ((int) msgId > getServerId()) {
+
+                sendMessage(msg);
+
+            //If message id is less than server id, send new message with server id to succesor and set running to true
+            } else if ((int) msgId < getServerId() && !getIsRunning()) {
+
+                setIsRunning(true);
+                sendMessage("0" + String.valueOf(getServerId()));
+                
+            //If message id matches server id, new message declaring leader is sent to all servers
+            } else if (msgId == getServerId()) {
+                
+                sendMessage("1" + String.valueOf(msgId));
+            }
+        //Recieve leader message, update leader id and stop running 
+        } else {
+            
+            setLeader(msgId);
+            setIsRunning(false);
+ 
+            //quit the election and not send anything
+            //call something to set proxy to send to this server now
+//            proxySetPrimary(receivedServer.getServerUrl()); //todo TELL some proxy?
+        }
+ 
+    }
+
+    public void sendMessage(String msg) {
+        try {
+            Socket s = getSuccSocket();
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF(msg);
+            dout.flush();
+            dout.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
 }
