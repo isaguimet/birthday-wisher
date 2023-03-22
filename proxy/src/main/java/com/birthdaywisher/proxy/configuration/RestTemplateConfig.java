7960a0be25b63e9bce0639d71edd61d908e97a30
sydneykwok <sydneykwok@gmail.com>
2023-03-21 22:48:02 -0600

    remove unnecessary async annotations

--- proxy/src/main/java/com/birthdaywisher/proxy/configuration/RestTemplateConfig.java
+++ proxy/src/main/java/com/birthdaywisher/proxy/configuration/RestTemplateConfig.java
@@ -3,11 +3,9 @@ package com.birthdaywisher.proxy.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@EnableAsync
 public class RestTemplateConfig {
 
     @Bean
