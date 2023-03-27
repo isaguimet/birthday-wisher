b7fea3ea342ed25ef343e880b04a616da431a87e
sydneykwok <sydneykwok@gmail.com>
2023-03-26 19:35:45 -0600

    rename files instead of changing code

--- client/src/Icons.js
+++ client/src/Icons.js
@@ -2,7 +2,7 @@ import homeIcon from './assets/home-icon.png';
 import friendsIcon from './assets/friends-icon.png';
 import notificationIcon from './assets/notification-icon.png';
 import profileIcon from './assets/profile-icon.png';
-import pfp from './assets/pfp-3.jpg';
+import pfp from './assets/pfp.jpg';
 import settingsIcon from './assets/settings.svg'
 
 export default [
